e464055982e7668ade232163222e693f1330bdf6
ppotatoG <thgus2270@gmail.com>
2022-06-20 23:37:52 +0900

    Refac IntersectionObserver

--- SECTION2/02/src/js.js
+++ SECTION2/02/src/js.js
@@ -3,18 +3,29 @@
     const target = document.querySelector('footer');
     let count = 0;
     const io = new IntersectionObserver((entry, observer) => {
-        if (!entry.isIntersecting && count < 10) {
-            io.unobserve(target);
-
-            gallery.appendChild(document.createElement('li'));
-            gallery.lastElementChild.textContent = `${count}`;
-
-            io.observe(target);
+        if (count < 10) {
+            setImg(gallery);
+            count++;
         }
-        count++;
+        else {
+            io.unobserve(target);
+            return false;
+        }
+
+        io.observe(target);
     }, {
         threshold: 0
     });
 
     io.observe(target);
 })();
+
+const setImg = (target) => {
+    const imgArr = ['../images/1.jpg', '../images/2.jpg', '../images/3.jpg'];
+
+    imgArr.forEach(src => {
+        const li = document.createElement('li');
+        li.innerHTML = `<figure><img src="${src}"></figure>`;
+        target.appendChild(li)
+    })
+}
