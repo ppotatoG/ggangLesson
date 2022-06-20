bc499cab38871157d58bee74ce4de3811a8225d0
ppotatoG <thgus2270@gmail.com>
2022-06-20 23:21:07 +0900

    Add IntersectionObserver

--- SECTION2/02/README.md
+++ SECTION2/02/README.md
@@ -1,1 +1,5 @@
 # 무한 스크롤
+
+## [new IntersectionObserver](https://developer.mozilla.org/ko/docs/Web/API/Intersection_Observer_API)
+
+[Intersection Observer - 요소의 가시성 관찰](https://heropy.blog/2019/10/27/intersection-observer/)
--- SECTION2/02/src/js.js
+++ SECTION2/02/src/js.js
@@ -1,51 +1,20 @@
 (() => {
     const gallery = document.querySelector('.sc_infinity .list');
+    const target = document.querySelector('footer');
+    let count = 0;
+    const io = new IntersectionObserver((entry, observer) => {
+        if (!entry.isIntersecting && count < 10) {
+            io.unobserve(target);
 
-    const imgEl = ['../images/1.jpg', '../images/2.jpg', '../images/3.jpg'];
-    let listCount = 0;
+            gallery.appendChild(document.createElement('li'));
+            gallery.lastElementChild.textContent = `${count}`;
 
-    function getList() {
-        let list = '';
-        listCount++;
-
-        if(listCount > 10) {
-            list = null;
-        }else{
-            imgEl.forEach(src => {
-                list += `<li><figure><img src="${src}"></figure></li>`;
-            })
+            io.observe(target);
         }
+        count++;
+    }, {
+        threshold: 0
+    });
 
-        return list;
-    }
-
-    const listCall = () => {
-        const winTop = window.scrollY;
-        const onTop = document.documentElement.getBoundingClientRect().height - window.innerHeight - document.querySelector('.footer').offsetHeight;
-
-        if(winTop >= onTop){
-            const data = getList();
-            if(data) {
-                const el = document.createElement('div');
-                el.innerHTML += data;
-
-                const arr = [];
-                arr.push.apply(arr, el.children);
-                arr.forEach(function(element) {
-                    gallery.appendChild(element);
-                });
-            }else{
-                return false;
-            }
-        }
-
-    }
-
-    const init = () => listCall();
-
-    window.addEventListener('scroll', function() {
-        listCall();
-    }, false);
-
-    init();
+    io.observe(target);
 })();
