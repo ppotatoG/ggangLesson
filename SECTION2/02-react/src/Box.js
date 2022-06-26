726429dee9484c75287159364ccbf59c234b7f0d
ppotatoG <thgus2270@gmail.com>
2022-06-26 19:57:17 +0900

    임시커밋

--- SECTION2/02-react/src/Box.js
+++ SECTION2/02-react/src/Box.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Box = () => {
+    const [ScrollY, setScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
+    const handleFollow = () => setScrollY(window.pageYOffset); // window 스크롤 값을 ScrollY에 저장
+
+    useEffect(() => {
+        console.log("ScrollY is ", ScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
+
+    }, [ScrollY])
+
+    useEffect(() => {
+        const watch = () => {
+            window.addEventListener('scroll', handleFollow);
+        }
+        watch(); // addEventListener 함수를 실행
+        return () => {
+            window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
+        }
+    })
+
+    const io = new IntersectionObserver((entry, observer) => {
+
+    }, {
+        threshold: 0
+    });
+
     const imgArr = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'];
     const list = imgArr.map((src, idx) => {
         return (
--- SECTION2/02-react/src/index.js
+++ SECTION2/02-react/src/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import useIntersect from "./useIntersect";
 
-import Box from './Box';
-import './index.css';
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <>
-        <h2 className="com_tit">INFINITE SCROLL</h2>
-        <Box />
-    </>
+const fakeFetch = (delay = 1000) => new Promise(res => setTimeout(res, delay));
+
+const ListItem = ({ number }) => (
+    <div className="ListItem">
+        <span>{number}</span>
+    </div>
 );
+
+function App() {
+    const [state, setState] = useState({ itemCount: 0, isLoading: false });
+
+    /* fake async fetch */
+    const fetchItems = async () => {
+        setState(prev => ({ ...prev, isLoading: true }));
+        await fakeFetch();
+        setState(prev => ({
+            itemCount: prev.itemCount + 10,
+            isLoading: false
+        }));
+    };
+
+    /* initial fetch */
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const [_, setRef] = useIntersect(async (entry, observer) => {
+        observer.unobserve(entry.target);
+        await fetchItems();
+        observer.observe(entry.target);
+    }, {});
+
+    const { itemCount, isLoading } = state;
+
+    if (!itemCount) return null;
+
+    return (
+        <div className="App">
+            {[...Array(itemCount)].map((_, i) => {
+                return <ListItem key={i} number={i} />;
+            })}
+            <div ref={setRef} className="Loading">
+                {isLoading && "Loading..."}
+            </div>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
