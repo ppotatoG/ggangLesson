19a49e357935b5ecea02875cf1d9926ba13fb392
ppotatoG <thgus2270@gmail.com>
2022-07-03 20:02:03 +0900

    스크롤 인터렉션 스크립트 작성하기

--- SECTION4/src/js.js
+++ SECTION4/src/js.js
@@ -1,26 +1,66 @@
-(() => {
-    const getPercent = () => {
-        const scrollHeight = document.querySelector('.sec01').offsetHeight;
-        const scrollRealHeight = scrollHeight - window.innerHeight;
-        const winScrollTop = window.scrollY;
-        const scrollPercent = Math.round((winScrollTop / scrollRealHeight) * 100);
+$(function(){
+    var scrollBody = $('.motion_area');
+    var parallaxDistance = 210;
 
-        render(scrollPercent);
-    }
+    var bgContent = scrollBody.find('.bg');
+    var ggangBody = scrollBody.find('.motion_ggang');
+    var moonBody = scrollBody.find('.motion_moon');
 
-    const text = document.querySelector('.progress .txt');
-    const progressBar = document.querySelector('.progress .bar');
+    var scrollHeight;
+    var scrollRealHeight;
+    var winScrollTop;
+    var percent;
+    var moveDistance;
 
-    const render = (scrollPercent) => {
-        text.innerText = `${scrollPercent}%`;
-        progressBar.style.width = `${scrollPercent}%`;
-    }
+    function setProperty() {
+        scrollHeight = scrollBody.height();
+        scrollRealHeight = (scrollHeight - $(window).height());
+        winScrollTop = $(window).scrollTop();
+        var scrollPerecnt = winScrollTop / scrollRealHeight;
+        percent = scrollPerecnt * 100;
+        moveDistance = scrollPerecnt * parallaxDistance;
+    };
 
-    const init = () => getPercent();
+    function motionGgang() {
+        setProperty();
+        changeBackgound();
+        parallaxMove();
+    };
 
-    document.addEventListener('scroll', function() {
-        getPercent();
-    }, false);
+    function changeBackgound() {//
+        if(percent < 25){
+            setBackground(0);
+        }else if(percent >= 25 && percent < 50){
+            setBackground(1);
+        }else if(percent >= 50 && percent < 75){
+            setBackground(2);
+            moonBody.removeClass('active');
+        }else if(percent >= 75 && percent < 100){
+            setBackground(3);
+            moonBody.addClass('active');
+
+        }
+    };
+
+    function setBackground(index) {
+        bgContent.removeClass('active');
+        bgContent.eq(index).addClass('active');
+    };
+
+    function parallaxMove() {
+        ggangBody.css({
+            transform : 'translate(0px,'+ moveDistance +'px)'
+        });
+    };
+
+    function init() {
+        motionGgang();
+    };
+
+    $(window).scroll(function(e) {
+        motionGgang();
+    });
 
     init();
-})();
+
+});
