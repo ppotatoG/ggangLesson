32293ef8cc3a0715afc09ee94ddceb6b3e1f2880
ppotaotG <thgus2270@gmail.com>
2022-07-08 19:46:36 +0900

    Change vanilla

--- SECTION5/src/js.js
+++ SECTION5/src/js.js
@@ -1,154 +1,160 @@
-$(function(){
-    /*변수및 요소 선언*/
+window.onload = function() {
 
-    var header = $('.header_wrap');
-    var sectionMainVisual = $('.sec_mainvis');
-    var sectionOverlap = $('.sec_list_overlap');
-    var sectionMainTop; //섹션의 top값을 구함.
-    var sectionMainBottom; //섹션의 BOTTOM값을 구함.
+    var header = document.querySelector('.header_wrap');
+    var sectionMainVisual = document.querySelector('.sec_mainvis');
+    var sectionOverlap = document.querySelector('.sec_list_overlap');
+    var sectionMainTop;
+    var sectionMainBottom;
 
-    var parallaxBody = $('.sec_list_overlap'); //패럴럭스가 시작될 엘리먼트 지정
-    var parallaxList = $('.sec_parallax .img_box'); //변수에 페럴럭스에 반응할 이미지 리스트틀 지정합니다.
+    var winScrollTop;
+    var sectionIsMoving = false;
 
-    /*리사이즈, 스크롤 할 때 변해야 할 값들*/
-    var winScrollTop; //스크롤 위치를 담을 변수 선언
-    var sectionIsMoving = false; //섹션이 이동 중인지 체크하는 변수
+    var parallaxBody = document.querySelector('.sec_list_overlap');
+    var parallaxList = document.querySelectorAll('.sec_parallax .img_box');
 
-    var parallaxOffsetTop; //패럴럭스가 시작될 요소의 상단 위치값
-    var parallaxThisTop; //패럴럭스가 시작될 위치값을 구함
-    var parallaxSpeed = 1200; // 패럴럭스 요소의 스피드
-    var parallaxPercent; // 패럴럭스 백분율값을 담을 변수를 선업합니다
-    var parallaxStartValue = 1000; //패럴럭스요소가 200 위치에서 시작하도록 설정합니다.
-    var parallaxMoveDistance; // 패럴럭스 요소가 움직일 거리를 담을 변수 선업합니다
+    var parallaxOffsetTop;
+    var parallaxThisTop;
+    var parallaxSpeed = 1200;
+    var parallaxPercent;
+    var parallaxStartValue = 1000;
+    var parallaxMoveDistance;
     var isMac = navigator.platform.indexOf('Mac') >= 0;
     var isEdge = /Edg/.test(navigator.userAgent);
 
-    function setProperty(){ //스크롤 할 때 변할 값들을 세팅하는 함수
+    function setProperty() {
 
-        winScrollTop = $(window).scrollTop(); //스크롤바의 현재 위치를 구함
-        sectionMainTop = sectionMainVisual.offset().top; //섹션의 top값을 구함.
-        sectionMainBottom = sectionMainTop + sectionMainVisual.height(); //섹션의 BOTTOM값을 구함.
+        winScrollTop = window.pageYOffset;
+        sectionMainTop = sectionMainVisual.getBoundingClientRect().top + winScrollTop;
+        sectionMainBottom = sectionMainTop + sectionMainVisual.offsetHeight;
 
-        parallaxOffsetTop = parallaxBody.offset().top; //패럴럭스가 시작될 요소의 상단 위치값
-        parallaxThisTop = winScrollTop - parallaxOffsetTop; //패럴럭스가 시작될 위치값을 구함.
-        parallaxPercent = parallaxThisTop / parallaxSpeed * 100; // 이동할 거리 백분율 값을 담음
-        parallaxMoveDistance = Math.max(parallaxStartValue - parallaxStartValue, Math.min(parallaxStartValue, parallaxStartValue - (parallaxStartValue * (parallaxPercent/100)))); //패럴럭스 요소가 움직일 거리를 구함
+        parallaxOffsetTop = parallaxBody.getBoundingClientRect().top + winScrollTop;
+        parallaxThisTop = winScrollTop - parallaxOffsetTop;
+        parallaxPercent = parallaxThisTop / parallaxSpeed * 100;
+        parallaxMoveDistance = Math.max(parallaxStartValue - parallaxStartValue, Math.min(parallaxStartValue, parallaxStartValue - (parallaxStartValue * (parallaxPercent/100))));
+
     };
 
-    function motionParallax() { // 스크롤 할 때 계속 호출될 패럴럭스 함수 선언
+    function motionParallax() {
 
-        if(parallaxPercent > 60) {
-            $('.sec_parallax').addClass('active');
-        } else {
-            $('.sec_parallax').removeClass('active');
+        if(parallaxPercent > 60){
+            document.querySelector('.sec_parallax').classList.add('active');
+        }else {
+            document.querySelector('.sec_parallax').classList.remove('active');
         }
 
-        parallaxList.eq(0).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance +'px)'
-        });
+        parallaxList[0].style.transform = 'translate(0px,'+ parallaxMoveDistance +'px)';
 
-        parallaxList.eq(1).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 2.1 +'px)'
-        });
+        parallaxList[1].style.transform = 'translate(0px,'+ parallaxMoveDistance * 2.1 +'px)';
 
-        parallaxList.eq(2).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 2.5 +'px)'
-        });
+        parallaxList[2].style.transform = 'translate(0px,'+ parallaxMoveDistance * 2.5 +'px)';
 
-        parallaxList.eq(3).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 3.5 +'px)'
-        });
+        parallaxList[3].style.transform = 'translate(0px,'+ parallaxMoveDistance * 3.5 +'px)';
 
-        parallaxList.eq(4).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 4.2 +'px)'
-        });
+        parallaxList[4].style.transform = 'translate(0px,'+ parallaxMoveDistance * 4.2 +'px)';
 
-        parallaxList.eq(5).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 4.8 +'px)'
-        });
+        parallaxList[5].style.transform = 'translate(0px,'+ parallaxMoveDistance * 4.8 +'px)';
 
-        parallaxList.eq(6).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 5.3 +'px)'
-        });
+        parallaxList[6].style.transform = 'translate(0px,'+ parallaxMoveDistance * 5.3 +'px)';
 
-        parallaxList.eq(7).css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ parallaxMoveDistance * 2.7 +'px)'
-        });
+        parallaxList[7].style.transform = 'translate(0px,'+ parallaxMoveDistance * 2.7 +'px)';
 
-        $('.sec_parallax .bg_line').css({ //계산된 값을 엘리먼트에 적용
-            transform : 'translate(0px,'+ -parallaxMoveDistance*3 +'px)'
-        });
+        document.querySelector('.sec_parallax .bg_line').style.transform = 'translate(0px,'+ -parallaxMoveDistance*3 +'px)';
+
     };
 
-    function moveSection(delta) { //스크롤 할 때 호출함
+    function moveSection(delta) {
 
         var deltaY = delta || null;
 
-        setProperty(); //스크롤 할 때 변해야 할 값들의 변수를 선언한 함수
-        motionParallax(); //패럴럭스 처리 함수
+        setProperty();
+        motionParallax();
 
         if(deltaY && isMac && winScrollTop >= sectionMainTop && winScrollTop < sectionMainBottom ||
             deltaY && !isMac && winScrollTop > sectionMainTop && winScrollTop < sectionMainBottom
         ) {
 
-            if(!sectionIsMoving) { //애니메이션이 진행 중인지 체크합니다.
-                if(!isEdge) {
+            if(!sectionIsMoving){ //애니메이션이 진행중인지 체크합니다.
+                if(!isEdge){
                     sectionIsMoving = true;
                 }
-                moveStartRender(deltaY); //섹션 이동을 처리하는 함수
+                moveStartRender(deltaY); //섹션 이동을 처리해주는 함수
             }
+
         }
 
-        if( winScrollTop >= sectionMainBottom) {
-            activeCehck(); //새로고침을 할 때 페이지가 아래에서 시작할 경우 액티브돼야 할 요소들을 처리
+        if(winScrollTop >= sectionMainBottom) {
+            activeCehck();
         }
+
     };
 
-    function activeCehck() { //새로고침을 할 때 페이지가 아래에서 시작할 경우 액티브돼야 할 요소들을 처리
+    function activeCehck() {
 
-        header.addClass('active');
-        sectionMainVisual.addClass('active');
-        sectionOverlap.addClass('active');
+        header.classList.add('active');
+        sectionMainVisual.classList.add('active');
+        sectionOverlap.classList.add('active');
     };
 
-    function moveStartRender(deltaY) { //섹션 이동 처리 함수
+    function moveStartRender(deltaY) {
 
-        if(deltaY > 0) { //해더 클래스가 없을 경우에는 아래로 내려오는 상황
+        if(deltaY > 0) {
 
-            header.addClass('active');
-            sectionMainVisual.addClass('active');
-            sectionOverlap.addClass('active');
+            header.classList.add('active');
+            sectionMainVisual.classList.add('active');
+            sectionOverlap.classList.add('active');
 
             if(!isEdge) {
-                $('html').stop(true).animate({
-                    scrollTop: sectionMainBottom+1 //IE버그 반복 버그 처리를 위해 1을 추가.
-                },500,function(){
-                    sectionIsMoving = false; //섹션이 이동 중인지 체크하는 변수
-                });
+                scrollMove(sectionMainBottom+1);
             }
 
-        } else { //해더 클래스가 있을 경우 위로 올라가는 상황
+        } else {
 
-            header.removeClass('active');
-            sectionMainVisual.removeClass('active');
-            sectionOverlap.removeClass('active');
+            header.classList.remove('active');
+            sectionMainVisual.classList.remove('active');
+            sectionOverlap.classList.remove('active');
 
             if(!isEdge) {
-                $('html').stop(true).animate({
-                    scrollTop: sectionMainTop
-                },500,function(){
-                    sectionIsMoving = false; //섹션이 이동 중인지 체크하는 변수
-                });
+                scrollMove(sectionMainTop);
             }
         }
+
     };
 
-    function init() { //초기화
+    function scrollMove(moveY) {
+
+        var speed = 3;
+        var vy = 0;
+        var scrollY = 0;
+
+        var loop = setInterval(function() {
+
+            var dir = moveY > window.pageYOffset ? 1 : -1;
+            vy += speed * dir;
+
+            if(dir > 0) {
+                scrollY = Math.min(moveY, window.pageYOffset + vy)
+            } else {
+                scrollY = Math.max(moveY, window.pageYOffset + vy)
+            }
+
+            window.scrollTo(0, scrollY);
+
+            if(scrollY >= moveY && dir > 0) {
+                sectionIsMoving = false;
+                clearInterval(loop);
+            } else if(scrollY <= moveY && dir < 0) {
+                sectionIsMoving = false;
+                clearInterval(loop);
+            }
+        }, 10);
+    };
+
+    function init(){
 
         moveSection();
     };
 
-    if(isMac) { //맥일경우
+    if(isMac) {
         window.addEventListener('wheel',function(e) {
 
             if(winScrollTop >= sectionMainTop && winScrollTop < sectionMainBottom) {
@@ -159,21 +165,21 @@ $(function(){
             moveSection(delta);
 
         }, {passive: false});
-    } else { //맥이 아닐 경우
+    } else {
         var beforeScroll = -1;
-        $(window).scroll(function(){
+        window.addEventListener('scroll', function() {
 
             var delta = 0;
-            if(beforeScroll > $(window).scrollTop()) {
+            if(beforeScroll > window.pageYOffset) {
                 delta = -1
             }else {
                 delta = 1
             }
 
             moveSection(delta);
-            beforeScroll = $(window).scrollTop();
-        });
+            beforeScroll = window.pageYOffset;
+        }, false);
     }
 
-    init(); //초기화
-});
+    init();
+};
